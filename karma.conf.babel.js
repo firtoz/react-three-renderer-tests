3f183636f17272d3bd7bc3755da27c50c345551b
Firtina Ozbalikci <firtina14@gmail.com>
2015-11-19 18:16:26 +0000

    [tests] Try to run normal chrome

--- karma.conf.babel.js
+++ karma.conf.babel.js
@@ -61,7 +61,17 @@ export default (config) => {
     configuration.customLaunchers = {
       Chrome_travis_ci: {
         base: 'Chrome',
-        flags: ['--no-sandbox --use-gl=osmesa --enable-logging=stderr --v=1'],
+        flags: [
+          '--use-gl=osmesa',
+          '--user-data-dir=~/tmp/x',
+          '--no-sandbox',
+          '--use-gl=osmesa',
+          '--enable-logging=stderr',
+          '--v=1',
+          '--no-first-run',
+          '--noerrdialogs',
+          '--disable-web-security',
+        ],
       },
     };
 
